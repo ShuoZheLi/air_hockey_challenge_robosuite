09f139eda99d0a52bd019973d9ae9059dc1936d7
Evan Kuo <evan.kuo.edu@gmail.com>
2024-04-04 20:19:38 -0500

    simple script to check stats of data

---
+++ datasetExamine.py
@@ -0,0 +1,40 @@
+import numpy as np
+import math
+import os
+
+from colorama import Fore, Style
+
+'''
+DATA FORMAT
+
+data: (mouse_x, mouse_y, puck_pos, puck_vel, gripper_pos, gripper_vel, joint_pos, joint_vel)
+
+{'puck_pos': array([0.79894989, 0.1171991 , 1.19990523]),
+ 'puck_vel': array([-0.04367879,  0.        , -0.00394174]),
+ 'gripper_pos': array([0.20976306, 0.00388718, 1.19852653]),
+ 'gripper_vel': array([-2.25057362,  0.09408171, -1.32536082]),
+ 'joint_pos': [-0.2562968815786609, -1.4037016841276384, 1.9696240174529724, -2.1944579935531774, -1.539114583987515, -2.044264439362114],
+ 'joint_vel': [-0.8948479668025617, -3.1744521848935854, 7.548211552338913, -3.4043974127948538, -0.1803107993539623, -12.451709298965682]}
+
+info["puck_pos"] = self.sim.data.get_body_xpos("puck")
+info["puck_vel"] = self.sim.data.get_body_xvelp("puck")
+info["gripper_pos"] = self.sim.data.site_xpos[self.robots[0].eef_site_id]
+info["gripper_vel"] = self.sim.data.get_body_xvelp("gripper0_eef")
+self.robot_joints = self.robots[0].robot_model.joints
+self._ref_joint_pos_indexes = [self.sim.model.get_joint_qpos_addr(x) for x in self.robot_joints]
+self._ref_joint_vel_indexes = [self.sim.model.get_joint_qvel_addr(x) for x in self.robot_joints]
+info["joint_pos"] = [self.sim.data.qpos[x] for x in self._ref_joint_pos_indexes]
+info["joint_vel"] = [self.sim.data.qvel[x] for x in self._ref_joint_vel_indexes]
+'''
+
+files = sorted(os.listdir('./Datasets'))
+
+totalSize = 0
+
+for filename in files:
+    if filename.endswith('.npy'):
+        data = np.load(f'./Datasets/{filename}')
+        totalSize += len(data)
+        print(f'{Style.RESET_ALL}Dataset Size: {Fore.GREEN}{len(data):<10} {Style.RESET_ALL}Filename: {Fore.RED}{filename}{Style.RESET_ALL}') # fancy colors
+print("-------------------------------------------------")
+print(f'{Style.RESET_ALL}Total Size: {Fore.BLUE}{totalSize} {Style.RESET_ALL}(Desired Size: {Fore.YELLOW}{999 * 300}{Style.RESET_ALL})') # fancy colors
