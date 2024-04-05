ce28f214cf8166a6effe8cee6c02ef9c64dfaeef
Evan Kuo <evan.kuo.edu@gmail.com>
2024-04-04 21:23:32 -0500

    sorts more accurately and displays sizes better

--- datasetExamine.py
+++ datasetExamine.py
@@ -27,7 +27,18 @@ info["joint_pos"] = [self.sim.data.qpos[x] for x in self._ref_joint_pos_indexes]
 info["joint_vel"] = [self.sim.data.qvel[x] for x in self._ref_joint_vel_indexes]
 '''
 
-files = sorted(os.listdir('./Datasets'))
+def sort_key(filename):
+    # Split the filename on underscores
+    parts = filename.split('_')
+
+    # Get the dataset number and the timestamp
+    dataset_number = int(parts[1])
+    timestamp = parts[-1].split('.')[0]
+
+    # Return a tuple of the dataset number and the timestamp
+    return (dataset_number, timestamp)
+
+files = sorted(os.listdir('./Datasets'), key=sort_key)
 
 totalSize = 0
 
@@ -37,4 +48,5 @@ for filename in files:
         totalSize += len(data)
         print(f'{Style.RESET_ALL}Dataset Size: {Fore.GREEN}{len(data):<10} {Style.RESET_ALL}Filename: {Fore.RED}{filename}{Style.RESET_ALL}') # fancy colors
 print("-------------------------------------------------")
-print(f'{Style.RESET_ALL}Total Size: {Fore.BLUE}{totalSize} {Style.RESET_ALL}(Desired Size: {Fore.YELLOW}{1000 * 300}{Style.RESET_ALL})') # fancy colors
+# print(f'{Style.RESET_ALL}Total Size: {Fore.BLUE}{totalSize} {Style.RESET_ALL}(Desired Size: {Fore.YELLOW}{1000 * 300}{Style.RESET_ALL})') # fancy colors
+print(f'{Style.RESET_ALL}Total Current Size vs Desired: {Fore.BLUE}{format(totalSize, ",")}{Style.RESET_ALL} / {Fore.YELLOW}{format(1000 * 300, ",")}{Style.RESET_ALL}') # fancy colors
